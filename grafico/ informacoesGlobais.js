3fcd81ead6824a8bac67e5129a2687c14cff57a9
Luiz felipe de sa ferreira <111285128+luizfelipedesa@users.noreply.github.com>
2024-09-30 11:01:03 -0300

    euuu

--- grafico/ informacoesGlobais.js
+++ grafico/ informacoesGlobais.js
@@ -1,9 +1,15 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = https://github.com/luizfelipedesa/3AEDITHGRAFICOS?tab=readme-ov-file#3aedithgraficos
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
-    const dados = await res.json()
-    console.log(dados);
-}
+    const dados await res.json()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+   paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
 
-vizualizarInformacoesGlobais()
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
+  }
+  
+  vizualizarInformacoesGlobais()
