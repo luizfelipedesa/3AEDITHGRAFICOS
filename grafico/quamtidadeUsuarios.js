402c51400504b9d7b58aa5931fe4984fd17410e9
Luiz felipe de sa ferreira <111285128+luizfelipedesa@users.noreply.github.com>
2024-09-30 11:42:44 -0300

    euuu

---
+++ grafico/quamtidadeUsuarios.js
@@ -0,0 +1,10 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+      
+      console.log(dados)
+  }
+  quantidadeUsuarios()
